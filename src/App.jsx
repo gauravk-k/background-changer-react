f2e3119b872c4a2a65a0ef1375dfe90a603a0f12
Gaurav <gauravkumarkashyap74354@gmail.com>
2023-09-19 22:49:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color,setColor]=useState("olive")
+  return (
+    <>
+      <div className="w-full h-screen duration-200"
+        style={{backgroundColor:color}}
+      >
+        <div className="h-screen fixed flex flex-wrap justify-center items-center inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center mx-auto gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+            <button 
+            onClick={()=>setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+
+
+            <button 
+            onClick={()=>setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+
+            <button 
+            onClick={()=>setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}}>Blue</button>
+
+            <button 
+            onClick={()=>setColor("aqua")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"aqua"}}>Aqua</button>
+
+            <button 
+            onClick={()=>setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"yellow"}}>Yellow</button>
+
+            <button 
+            onClick={()=>setColor("purple")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"purple"}}>Purple</button>
+
+            <button 
+            onClick={()=>setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>Black</button>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
